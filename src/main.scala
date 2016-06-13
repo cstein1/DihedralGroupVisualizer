ec109d52e7e37370e1bfaeca1b452f86ca20223c
Charlie Stein <cstein1@trinity.edu>
2016-06-13 02:26:17 -0500

    Cleaning up

--- src/main.scala
+++ src/main.scala
@@ -6,124 +6,43 @@ import scalafx.scene.shape._
 import scalafx.scene.text._
 import scalafx.event.ActionEvent
 import scalafx.event.EventHandler
+import scalafx.scene.layout.HBox
 
 import scala.collection.mutable.ListBuffer
 
-object Main {
-   def TestEachIndividually: Unit = 
-   {
-     var n = 10
-     var Example = new NGon(n)
-     Example.Display()
-     println("Rotate Left")
-     for (a <- 0 until n)
-     {
-       Example.RotateLeft
-       Example.Display()
-     }
-     println("Rotate Right")
-     for (a <- 0 until n)
-     {
-       Example.RotateRight
-       Example.Display()
-     }
-     println("Flips")
-     for(a <- 0 until 4)
-     {
-       Example.Flip
-       Example.Print
-     }
-     println("Generate Group")
-     for(a <- 0 until n)
-     {
-       Example.RotateRight
-       Example.Print
-       Example.Flip
-       Example.Print
-       Example.Flip
-     }
-   }
-   
-   def TestBinaryOperations:Unit =
-   {
-     var rot1 = new Rotations(1)
-     var rot2 = new Rotations(2)
-     var flip1 = new Flips(4)
-     var flip2 = new Flips(5)
-     var rotsList = ListBuffer[Permutations](rot1, rot2)
-     var flipsList = ListBuffer[Permutations](flip1, flip2)
-     var mixList1 = ListBuffer[Permutations](rot1,flip1)
-     var mixList2 = ListBuffer[Permutations](flip1,rot1)
-     
-     var rotState = new NGon(3).state     
-     var flipState = new NGon(3).state
-     var mixState1 = new NGon(3).state
-     var mixState2 = new NGon(3).state
-     
-     rotState.+=(rotsList)
-     flipState.+=(flipsList)
-     mixState1.+=(mixList1)
-     mixState2.+=(mixList2)
-     
-     println("Rot + Rot")
-     rotState.PrintState()
-     rotState.ReduceState(0)
-     rotState.PrintState()
-     
-     println("Flip + Flip")
-     flipState.PrintState()
-     flipState.ReduceState(0)
-     flipState.PrintState()
-     
-     println("Rot + Flip")
-     mixState1.PrintState()
-     mixState1.ReduceState(0)
-     mixState1.PrintState()
-     
-     println("Flip + Rot")
-     mixState2.PrintState()
-     mixState2.ReduceState(0)
-     mixState2.PrintState()
-   }
-     
-   def TestUpdate = {
-     var n = 4
-     var Example = new NGon(n)
-     Example.Print
-     Example.RotateRight
-     Example.RotateLeft
-     Example.Print
-     Example.Flip
-     Example.Flip
-     Example.RotateLeft
-     Example.Print
-     Example.RotateRight
-     Example.Print
-     Example.Flip
-     Example.Print
-   }
-   
-  def Trying2BreakIt = {
-     var n = 4
-     var Example = new NGon(n)
-     Example.Print
-     Example.Flip
-     Example.Print
-     Example.RotateLeft
-     Example.Print
-     Example.Flip
-     Example.Print
-     Example.RotateLeft
-     Example.Print
-  }
-   
-  
+object Main {  
     var n = 10
     var ngon = new NGon(n)
     ngon.Print
     var Points = List[Double]()
     
 
+    def restart(args:Array[String]):Unit = {
+//      val app = new JFXApp {
+//        stage = new JFXApp.PrimaryStage{
+//            scene = new Scene(100,100) {
+//            println("clicked")
+//            val label1 = new Label("Enter the number of sides you want: ")
+//            val textField = new TextField
+//            val hb = new HBox()
+//            hb.getChildren.addAll(label1, textField)
+//            hb.setSpacing(10)
+//            val button = new Button("OK")
+//            button.onAction = (e:ActionEvent) => {
+//              if(textField.text == "\\d*"){ 
+//                n = 1
+//                ngon = new NGon(n)
+//                GUI(args)
+//              }
+//              else{
+//               textField.text = "Please enter an integer" 
+//              }
+//            }
+//          }
+//        }
+//      }
+      print("hi")
+    }
     
   def GUI(args: Array[String]):Unit = {
     val app = new JFXApp {
@@ -136,6 +55,7 @@ object Main {
           exitItem.onAction = (e:ActionEvent) => sys.exit(0)
           val editMenu = new Menu("Interact")
           val numSides = new MenuItem("Change Number of Sides")
+//          numSides.onAction = (e:ActionEvent) => restart(args)
           
           fileMenu.items = List(exitItem)
           editMenu.items = List(numSides)
@@ -223,11 +143,6 @@ object Main {
   
   def main(args: Array[String]): Unit = 
   {
-    //TestBinaryOperations
-    //TestEachIndividually
-    //TestUpdate
-    //Trying2BreakIt
-    
     GUI(args)
   }
 }
